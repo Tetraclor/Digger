123f9ebbca4b4a0e4bcaf764a67eea8b4643f4f9
Даниил Королев <kdvikt@gmail.com>
2022-03-09 19:31:41 +0400

    Fix winforms animations

---
+++ WinFormsGameView/CreatureAnimation.cs
@@ -0,0 +1,91 @@
+﻿using GameCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace WinFormsGameView
+{
+    public class CreatureAnimation
+    {
+        public static readonly Dictionary<string, Bitmap> bitmaps = new Dictionary<string, Bitmap>();
+
+        public static List<CreatureAnimation> Animations = new();
+
+        public static int AnimationTickLength = 32;
+        public static int AnimationTickCount = 0;
+        public static int SpriteSize = 32;
+
+        public static int AnimationDelta { private set; get; }
+
+        static CreatureAnimation()
+        {
+            var imagesDirectory = new DirectoryInfo("../../../Images");
+
+            foreach (var e in imagesDirectory.GetFiles("*.png"))
+                bitmaps[e.Name] = (Bitmap)Image.FromFile(e.FullName);
+        }
+
+
+        public Bitmap Sprite;
+        public System.Drawing.Point DrawLocation;
+
+        private GameCore.Point Location;
+        private int DeltaX;
+        private int DeltaY;
+
+        public CreatureAnimation(CreatureTransformation transformation)
+        {
+            Sprite = bitmaps[CreatureImageNameGetter.Get((dynamic)transformation.Creature)];
+            DeltaX = transformation.Command.DeltaX;
+            DeltaY = transformation.Command.DeltaY;
+            Location = transformation.Location;
+        }
+
+        private CreatureAnimation()
+        {
+        }
+
+        public static void Tick(int mapWitdh, int mapHeight)
+        {
+            AnimationTickCount++;
+            AnimationDelta = (SpriteSize / AnimationTickLength) * (AnimationTickCount + 1);
+
+            for (int i = 0; i < Animations.Count; i++)
+            {
+                var animation = Animations[i];
+
+                var deltaX = animation.DeltaX;
+                var deltaY = animation.DeltaY;
+                var location = animation.Location;
+
+                if (Math.Abs(deltaX) == mapWitdh - 1 ||
+                    Math.Abs(deltaY) == mapHeight - 1) //Телепортация с конца карты на другой конец
+                {
+                    deltaX = -Math.Sign(deltaX);
+                    deltaY = -Math.Sign(deltaY);
+
+                    var fakeAnimation = new CreatureAnimation()
+                    {
+                        Sprite = animation.Sprite,
+                        Location = new GameCore.Point(
+                            location.X + animation.DeltaX - deltaX, 
+                            location.Y + animation.DeltaY - deltaY),
+                        DeltaX = deltaX,
+                        DeltaY = deltaY,
+                    };
+
+                    Animations.Add(fakeAnimation);
+                }
+
+                var temp = new System.Drawing.Point(
+                    location.X * SpriteSize + AnimationDelta * deltaX,
+                    location.Y * SpriteSize + AnimationDelta * deltaY);
+
+                animation.DrawLocation = temp;
+            }
+
+            if (AnimationTickCount == AnimationTickLength) AnimationTickCount = 0;
+        }
+    }
+}
--- WinFormsGameView/GameWindow.cs
+++ WinFormsGameView/GameWindow.cs
@@ -5,7 +5,6 @@ using GameSnake;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
@@ -14,14 +13,10 @@ namespace WinFormsGameView
 {
     public class GameWindow : Form
     {
-        private static int ElementSize = 32;
        // private GameService gameService = new DiggerGameService(DiggerGameService.mapWithPlayerTerrain);
         private GameService gameService = new SnakeGameService(10, 10);
 
-
         private readonly HashSet<Keys> pressedKeys = new HashSet<Keys>();
-
-
         private UserPlayer userPlayer = new UserPlayer();
 
         private int MapWidth;
@@ -29,6 +24,7 @@ namespace WinFormsGameView
 
         public GameWindow()
         {
+            CreatureAnimation.SpriteSize = 32;
             InitLocalGame();
             InitForm(MapWidth, MapHeight);
             StartGame(LocalGameTimerTick);
@@ -36,9 +32,11 @@ namespace WinFormsGameView
 
         void InitForm(int MapWidth, int MapHeight) 
         {
+            var elementSize = CreatureAnimation.SpriteSize;
+
             ClientSize = new Size(
-                    ElementSize * MapWidth,
-                    ElementSize * MapHeight + ElementSize);
+                    elementSize * MapWidth,
+                    elementSize * MapHeight + elementSize);
 
             FormBorderStyle = FormBorderStyle.FixedDialog;
         }
@@ -82,9 +80,11 @@ namespace WinFormsGameView
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            e.Graphics.TranslateTransform(0, ElementSize);
+            var elementSize = CreatureAnimation.SpriteSize;
+
+            e.Graphics.TranslateTransform(0, elementSize);
             e.Graphics.FillRectangle(
-                Brushes.Black, 0, 0, ElementSize * MapWidth, ElementSize * MapHeight);
+                Brushes.Black, 0, 0, elementSize * MapWidth, elementSize * MapHeight);
 
             for (int i = 0; i < CreatureAnimation.Animations.Count; i++)
             {
@@ -96,74 +96,6 @@ namespace WinFormsGameView
            // e.Graphics.DrawString(GameState.Scores.ToString(), new Font("Arial", 16), Brushes.Green, 0, 0);
         }
 
-        public class CreatureAnimation
-        {
-            public static readonly Dictionary<string, Bitmap> bitmaps = new Dictionary<string, Bitmap>();
-
-            public static List<CreatureAnimation> Animations = new();
-
-            public static int AnimationTickLength = 32;
-            public static int AnimationTickCount = 0;
-            public static int SpriteSize = ElementSize;
-
-            public static int AnimationDelta { private set; get; }
-
-            static CreatureAnimation()
-            {
-                var imagesDirectory = new DirectoryInfo("../../../Images");
-
-                foreach (var e in imagesDirectory.GetFiles("*.png"))
-                    bitmaps[e.Name] = (Bitmap)Image.FromFile(e.FullName);
-            }
-
-
-            public Bitmap Sprite;
-            public System.Drawing.Point DrawLocation;
-
-            public GameCore.Point Location;
-            public int DeltaX;
-            public int DeltaY;
-
-            public CreatureAnimation(CreatureTransformation transformation)
-            {
-                Sprite = bitmaps[CreatureImageNameGetter.Get((dynamic)transformation.Creature)];
-                DeltaX = transformation.Command.DeltaX;
-                DeltaY = transformation.Command.DeltaY;
-                Location = transformation.Location;
-            }
-
-            public static void Tick(int mapWitdh, int mapHeight)
-            {
-                AnimationTickCount++;
-                AnimationDelta = (ElementSize / AnimationTickLength) * (AnimationTickCount + 1);
-
-                foreach (var animation in Animations)
-                {
-                    var location = animation.Location;
-                    var deltaX = animation.DeltaX;
-                    var deltaY = animation.DeltaY;
-
-                    var temp = new System.Drawing.Point(
-                        location.X * SpriteSize + AnimationDelta * deltaX,
-                        location.Y * SpriteSize + AnimationDelta * deltaY);
-
-                    if (Math.Abs(deltaX) == mapWitdh - 1 ||
-                        Math.Abs(deltaY) == mapHeight - 1) //Телепортация с конца на другой конец
-                    {
-                        var xs = -Math.Sign(deltaX);
-                        var ys = -Math.Sign(deltaY);
-
-                        temp = new System.Drawing.Point(
-                            location.X * SpriteSize + AnimationDelta * xs, 
-                            location.Y * SpriteSize + AnimationDelta * ys);
-                    }
-
-                    animation.DrawLocation = temp;
-                }
-
-                if (AnimationTickCount == AnimationTickLength) AnimationTickCount = 0;
-            }
-        }
 
         private void LocalGameTimerTick(object sender, EventArgs args)
         {
@@ -176,7 +108,7 @@ namespace WinFormsGameView
                     .ToList();
             }
                
-            CreatureAnimation.Tick(gameService.GameState.MapWidth, gameService.GameState.MapHeight);
+            CreatureAnimation.Tick(MapWidth, MapHeight);
 
             Invalidate();
         }
