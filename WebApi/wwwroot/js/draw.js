9ad99b48910fe70d687bd709b55ecd854c13be7d
Даниил Королев <kdvikt@gmail.com>
2022-03-19 22:05:00 +0400

    canvas scale

--- WebApi/wwwroot/js/draw.js
+++ WebApi/wwwroot/js/draw.js
@@ -1,7 +1,5 @@
 ﻿async function get_images() {
-
     var animateInfo = await hubConnection.invoke("GetAnimateInfo");
-    console.log(animateInfo);
 
     var paths = animateInfo.mapCharToSprite;
 
@@ -15,30 +13,35 @@
     return images;
 }
 
-async function draw_board(canvas, data) {
-    // use the intrinsic size of image in CSS pixels for the canvas element
-    
+async function draw_board(canvas, data, maxw = 500, maxh=500) {
+
     if (window.images === undefined) {
         window.images = await get_images();
     }
 
     var images = window.images;
+
     var ctx = canvas.getContext('2d');
     var size = 32;
 
     var rows = data.split('\n');
 
-    console.log(rows);
+    var w = rows[0].length * size;
+    var h = rows.length * size;
 
-    canvas.width = rows[0].length * size;
-    canvas.height = (rows.length) * size;
+    var sw = maxw / w;
+    var sh = maxh / h;
+
+    canvas.width = Math.min(w, maxw);
+    canvas.height = Math.min(h, maxh);
 
     ctx.fillStyle = "rgb(0,0,0)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    ctx.scale(sw, sh);
+
     for (var y = 0; y < rows.length; y++) {
         var row = rows[y];
-
         for (var x = 0; x < row.length; x++) {
             var image = images[row[x]];
             if (image === undefined) continue;
